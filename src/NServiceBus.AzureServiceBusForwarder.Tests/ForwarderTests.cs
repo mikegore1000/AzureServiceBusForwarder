379f2809fdf19c32e669aa5c05643cf77134c615
Mike Gore <mikegore1000@msn.com>
2017-08-23 04:06:50 +0100

    Refactored tests

--- src/NServiceBus.AzureServiceBusForwarder.Tests/ForwarderTests.cs
+++ src/NServiceBus.AzureServiceBusForwarder.Tests/ForwarderTests.cs
@@ -7,13 +7,20 @@ namespace NServiceBus.AzureServiceBusForwarder.Tests
     [TestFixture]
     public class ForwarderTests
     {
+        private IEndpointInstance endpointFake;
+
+        [SetUp]
+        public void Setup()
+        {
+            endpointFake = A.Fake<IEndpointInstance>();
+        }
+
         [Test]
         [TestCase(null)]
         [TestCase("")]
         public void when_creating_a_forwarder_the_connection_string_is_required(string connectionString)
         {
-            var endpoint = A.Fake<IEndpointInstance>();
-            Assert.Throws<ArgumentException>(() => new Forwarder(connectionString, "TestTopic", "TestSubscriber", endpoint, message => typeof(TestMessage)));
+            Assert.Throws<ArgumentException>(() => new Forwarder(connectionString, "TestTopic", "TestSubscriber", endpointFake, message => typeof(TestMessage)));
         }
 
         [Test]
@@ -21,8 +28,7 @@ namespace NServiceBus.AzureServiceBusForwarder.Tests
         [TestCase("")]
         public void when_creating_a_forwarder_the_topic_name_is_required(string topicName)
         {
-            var endpoint = A.Fake<IEndpointInstance>();
-            Assert.Throws<ArgumentException>(() => new Forwarder("ConnectionString", topicName, "TestSubscriber", endpoint, message => typeof(TestMessage)));
+            Assert.Throws<ArgumentException>(() => new Forwarder("ConnectionString", topicName, "TestSubscriber", endpointFake, message => typeof(TestMessage)));
         }
     }
 

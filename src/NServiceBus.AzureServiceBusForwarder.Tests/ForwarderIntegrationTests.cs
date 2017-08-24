54f6043ef1bbaa9259d0b872ae085154f0e2c718
Mike Gore <mikegore1000@msn.com>
2017-08-24 04:53:23 +0100

    Split forwarder integration and unit tests

---
+++ src/NServiceBus.AzureServiceBusForwarder.Tests/ForwarderIntegrationTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using FakeItEasy;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using NUnit.Framework;
+
+namespace NServiceBus.AzureServiceBusForwarder.Tests
+{
+    public class ForwarderIntegrationTests
+    {
+        private IEndpointInstance endpointFake;
+
+        [SetUp]
+        public void Setup()
+        {
+            endpointFake = A.Fake<IEndpointInstance>();
+        }
+
+        [Test]
+        public async Task when_a_forwarder_is_started_messages_are_forwarded_via_the_endpoint()
+        {
+            const string topicName = "sourceTopic";
+            var namespaceConnectionString = Environment.GetEnvironmentVariable("NServiceBus.AzureServiceBusForwarder.ConnectionString", EnvironmentVariableTarget.User);
+            var namespaceManager = NamespaceManager.CreateFromConnectionString(namespaceConnectionString);
+
+            if (!await namespaceManager.TopicExistsAsync(topicName))
+            {
+                await namespaceManager.CreateTopicAsync(topicName);
+            }
+
+            var forwarder = new Forwarder(namespaceConnectionString, topicName, "destinationQueue", endpointFake, message => typeof(TestMessage));
+            await forwarder.Start();
+
+            var topicClient = TopicClient.CreateFromConnectionString(namespaceConnectionString, topicName);
+            var eventMessage = new BrokeredMessage(new TestMessage());
+            var tcs = new TaskCompletionSource<string>();
+            await topicClient.SendAsync(eventMessage);
+
+            A.CallTo(endpointFake).Invokes(() => tcs.SetResult("kfd"));
+
+            if (!tcs.Task.Wait(TimeSpan.FromSeconds(10)))
+            {
+                Assert.Fail("Timed out waiting for message to be forwarded");
+            }
+        }
+    }
+}
--- src/NServiceBus.AzureServiceBusForwarder.Tests/ForwarderTests.cs
+++ src/NServiceBus.AzureServiceBusForwarder.Tests/ForwarderTests.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using FakeItEasy;
-using Microsoft.ServiceBus;
 using NUnit.Framework;
-using System.Threading.Tasks;
-using Microsoft.ServiceBus.Messaging;
 
 namespace NServiceBus.AzureServiceBusForwarder.Tests
 {
@@ -52,34 +49,6 @@ namespace NServiceBus.AzureServiceBusForwarder.Tests
         public void when_creating_a_forwarder_a_message_mapper_is_required()
         {
             Assert.Throws<ArgumentNullException>(() => new Forwarder("ConnectionString", "TestTopic", "DestinationQueue", endpointFake, null));
-        }
-
-        [Test]
-        public async Task when_a_forwarder_is_started_messages_are_forwarded_via_the_endpoint()
-        {
-            const string topicName = "sourceTopic";
-            var namespaceConnectionString = Environment.GetEnvironmentVariable("NServiceBus.AzureServiceBusForwarder.ConnectionString", EnvironmentVariableTarget.User);
-            var namespaceManager = NamespaceManager.CreateFromConnectionString(namespaceConnectionString);
-            
-            if (!await namespaceManager.TopicExistsAsync(topicName))
-            {
-                await namespaceManager.CreateTopicAsync(topicName);
-            }
-
-            var forwarder = new Forwarder(namespaceConnectionString, topicName, "destinationQueue", endpointFake, message => typeof(TestMessage));
-            await forwarder.Start();
-
-            var topicClient = TopicClient.CreateFromConnectionString(namespaceConnectionString, topicName);
-            var eventMessage = new BrokeredMessage(new TestMessage());
-            var tcs = new TaskCompletionSource<string>();
-            await topicClient.SendAsync(eventMessage);
-
-            A.CallTo(endpointFake).Invokes(() => tcs.SetResult("kfd"));
-
-            if (!tcs.Task.Wait(TimeSpan.FromSeconds(10)))
-            {
-                Assert.Fail("Timed out waiting for message to be forwarded");
-            }
         }
 
         [Test]

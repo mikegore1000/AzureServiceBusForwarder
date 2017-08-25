29ba5bc27d528aaf531117fd32739b26741c2e96
Mike Gore <mikegore1000@msn.com>
2017-08-25 05:21:03 +0100

    Added missing files

---
+++ src/NServiceBus.AzureServiceBusForwarder.Tests/ForwarderDestinationConfigurationTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using FakeItEasy;
+using NUnit.Framework;
+
+namespace NServiceBus.AzureServiceBusForwarder.Tests
+{
+    [TestFixture]
+    public class ForwarderDestinationConfigurationTests
+    {
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        public void when_creating_the_destination_configuration_then_the_destination_queue_is_required(string destinationQueue)
+        {
+            Assert.Throws<ArgumentException>(() => new ForwarderDestinationConfiguration(destinationQueue, A.Fake<IEndpointInstance>()));
+        }
+
+        [Test]
+        public void when_creating_the_destination_configuration_then_the_endpoint_is_required()
+        {
+            Assert.Throws<ArgumentNullException>(() => new ForwarderDestinationConfiguration("DestinationQueue", null));
+        }
+    }
+}
---
+++ src/NServiceBus.AzureServiceBusForwarder/ForwarderDestinationConfiguration.cs
@@ -0,0 +1,18 @@
+﻿namespace NServiceBus.AzureServiceBusForwarder
+{
+    public class ForwarderDestinationConfiguration
+    {
+        public ForwarderDestinationConfiguration(string destinationQueue, IEndpointInstance endpoint)
+        {
+            Guard.IsNotNullOrEmpty(destinationQueue, nameof(destinationQueue));
+            Guard.IsNotNull(endpoint, nameof(endpoint));
+
+            DestinationQueue = destinationQueue;
+            Endpoint = endpoint;
+        }
+
+        public string DestinationQueue { get; }
+
+        public IEndpointInstance Endpoint { get; }
+    }
+}

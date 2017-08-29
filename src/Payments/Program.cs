d24973a9d96c31643112fb75460f58fb75dcc734
Mike Gore <mikegore1000@msn.com>
2017-08-29 20:38:56 +0100

    Tweaked payment prefetch count for faster send performance on the orders side.  Better for demos

--- src/Payments/Program.cs
+++ src/Payments/Program.cs
@@ -59,7 +59,7 @@ namespace Payments
 
             var endpoint = await Endpoint.Start(endpointConfig).ConfigureAwait(false);
             var forwarder = new Forwarder(
-                new ForwarderSourceConfiguration(ordersConnectionString, "Returns", 500, 4000),
+                new ForwarderSourceConfiguration(ordersConnectionString, "Returns", 500, 1000),
                 new ForwarderDestinationConfiguration("Payments", endpoint),
                 m => Type.GetType($"{(string)m.Properties["Asos.EnclosedType"]}, Payments"),
                 new NServiceBus.AzureServiceBusForwarder.Serializers.JsonSerializer());

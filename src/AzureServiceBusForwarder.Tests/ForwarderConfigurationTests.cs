4a566ee114b6999d2e56d542f980d43001bc3d02
Mike Gore <mikegore1000@msn.com>
2017-08-31 08:42:27 +0100

    Added forwarder configuration, accepting source config

---
+++ src/AzureServiceBusForwarder.Tests/ForwarderConfigurationTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AzureServiceBusForwarder.Tests
+{
+    [TestFixture]
+    public class ForwarderConfigurationTests
+    {
+        [Test]
+        public void when_creating_the_configuration_the_source_configuration_is_required()
+        {
+            Assert.Throws<ArgumentNullException>(() => new ForwarderConfiguration(null));
+        }
+    }
+}
---
+++ src/AzureServiceBusForwarder/ForwarderConfiguration.cs
@@ -0,0 +1,14 @@
+﻿namespace AzureServiceBusForwarder
+{
+    public class ForwarderConfiguration
+    {
+        private readonly ForwarderSourceConfiguration source;
+
+        public ForwarderConfiguration(ForwarderSourceConfiguration source)
+        {
+            Guard.IsNotNull(source, nameof(source));
+
+            this.source = source;
+        }
+    }
+}

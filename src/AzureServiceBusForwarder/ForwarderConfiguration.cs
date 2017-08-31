4e900dfd953e387ff98acf15133f461a7a5eee6e
Mike Gore <mikegore1000@msn.com>
2017-08-31 09:16:05 +0100

    Provided internal properties so the forwarder can access the ForwarderConfiguration data

--- src/AzureServiceBusForwarder/ForwarderConfiguration.cs
+++ src/AzureServiceBusForwarder/ForwarderConfiguration.cs
@@ -2,19 +2,21 @@
 {
     public class ForwarderConfiguration
     {
-        private readonly ForwarderSourceConfiguration source;
-        private readonly ForwarderDestinationConfiguration destination;
-
         public ForwarderConfiguration(ForwarderSourceConfiguration source, ForwarderDestinationConfiguration destination)
         {
             Guard.IsNotNull(source, nameof(source));
             Guard.IsNotNull(destination, nameof(destination));
 
-            this.source = source;
-            this.destination = destination;
+            this.Source = source;
+            this.Destination = destination;
         }
 
+        internal ForwarderSourceConfiguration Source { get; }
+
+        internal ForwarderDestinationConfiguration Destination { get; }
+
         internal ILogger Logger { get; private set; }
+
 
         public ForwarderConfiguration UsingLogger(ILogger logger)
         {
